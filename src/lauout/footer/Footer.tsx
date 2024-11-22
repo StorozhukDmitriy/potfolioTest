9f037b87131828235547f6e9b8c9470b91e503c2
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-11-22 23:47:33 +0300

    PortfolioDesktopOver

--- src/lauout/footer/Footer.tsx
+++ src/lauout/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Icon } from "../../components/icon/Icon";
+import { Theme } from "../../styles/Theme";
 
 export const Footer = () => {
   return (
@@ -10,9 +11,9 @@ export const Footer = () => {
           <SocialIconLink>
             <Icon
               iconId="instagramm"
-              width="28px"
-              height="28px"
-              viewBox="0 0 28 28"
+              width="21px"
+              height="21px"
+              viewBox="0 0 21 21"
             />
           </SocialIconLink>
         </SocialItem>
@@ -20,24 +21,24 @@ export const Footer = () => {
           <SocialIconLink>
             <Icon
               iconId="telegramm"
-              width="28px"
-              height="28px"
-              viewBox="0 0 28 28"
+              width="21px"
+              height="21px"
+              viewBox="0 0 21 21"
             />
           </SocialIconLink>
         </SocialItem>
         <SocialItem>
           <SocialIconLink>
-            <Icon iconId="vk" width="28px" height="28px" viewBox="0 0 28 28" />
+            <Icon iconId="vk" width="21px" height="21px" viewBox="0 0 21 21" />
           </SocialIconLink>
         </SocialItem>
         <SocialItem>
           <SocialIconLink>
             <Icon
               iconId="linkendin"
-              width="28px"
-              height="28px"
-              viewBox="0 0 28 28"
+              width="21px"
+              height="21px"
+              viewBox="0 0 21 21"
             />
           </SocialIconLink>
         </SocialItem>
@@ -48,20 +49,43 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  background-color: aqua;
-  min-height: 30vh;
+  background-color: ${Theme.colors.primaryBg};
   text-align: center;
+  margin: 40px 0;
 `;
-const Name = styled.span``;
+const Name = styled.span`
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 3px;
+`;
 const SocialList = styled.ul`
   display: flex;
   justify-content: center;
-  li + li {
-    margin-left: 10px;
+  gap: 20px;
+  margin: 30px 0;
+`;
+const SocialItem = styled.li``;
+
+const SocialIconLink = styled.a`
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.1);
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: ${Theme.colors.accent};
+  &:hover {
+    transform: translateY(-4px);
+    background-color: ${Theme.colors.accent};
+    color: ${Theme.colors.primaryBg};
   }
 `;
-const SocialItem = styled.li`
-  list-style: none;
+
+const Copyrigth = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  color: rgba(255, 255, 255, 0.5);
 `;
-const SocialIconLink = styled.a``;
-const Copyrigth = styled.small``;
--- src/lauout/sections/slogan/Slogan.tsx
+++ src/lauout/sections/slogan/Slogan.tsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/button/Button";
+import { Container } from "../../../components/Container";
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Slogan = () => {
   return (
     <StyledSlogan>
-      <SectionTitle>I Am Available For Freelance</SectionTitle>
-      <Button>Hire me</Button>
+      <Container>
+        <FlexWrapper direction="column" align="center">
+          <SectionTitle>I Am Available For Freelance</SectionTitle>
+          <Button>Hire me</Button>
+        </FlexWrapper>
+      </Container>
     </StyledSlogan>
   );
 };
